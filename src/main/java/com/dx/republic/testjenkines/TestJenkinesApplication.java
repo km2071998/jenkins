66605e8f8b4741e8e34aa22a102898168c9a78a9
kareem-mohamed207 <kareem.mohsamed@dx-republic.com>
2025-01-24 20:33:40 +0200

    first commit

---
+++ src/main/java/com/dx/republic/testjenkines/TestJenkinesApplication.java
@@ -0,0 +1,13 @@
+package com.dx.republic.testjenkines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJenkinesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestJenkinesApplication.class, args);
+    }
+
+}
